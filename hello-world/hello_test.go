4bc3953de98056128aa2291b063fd8b93fbdffff
Vicki Liou <liou.vicki@gmail.com>
2022-03-10 23:56:34 -0300

    refact: add default printing when empty string is passed

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Gopher")
-	want := "Hello, Gopher"
-
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	}
+
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Gopher")
+		want := "Hello, Gopher"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
