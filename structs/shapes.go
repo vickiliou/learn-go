3a1c72581c948c1ec47e99ea4e124f15a64ae660
Vicki Liou <liou.vicki@gmail.com>
2022-05-15 23:24:01 -0300

    refact: use method receiver to calculate the area of a rectangle add: method to calculate the area of a circle

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,16 @@
 package shapes
 
+import "math"
+
 // Rectangle has the dimensions of a rectangle.
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+// Area returns the area of the rectangle.
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 // Perimeter returns the perimeter of the rectangle.
@@ -11,7 +18,12 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-// Area returns the area of the rectangle.
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+// Circle has the dimensions of a circle.
+type Circle struct {
+	Radius float64
+}
+
+// Area returns the area of the circle.
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
