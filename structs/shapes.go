51cd4a3d3ba59cffb2649b86c21326c2d9ba8371
Vicki Liou <liou.vicki@gmail.com>
2022-05-18 23:20:51 -0300

    add: method to calculate the area of a triangle

--- structs/shapes.go
+++ structs/shapes.go
@@ -32,3 +32,14 @@ type Circle struct {
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
+
+// Triangle has the dimensions of a triangle.
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+// Area returns the area of the triangle.
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
