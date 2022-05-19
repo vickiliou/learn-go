0d608963adfa2030763350467c468f8328180da5
Vicki Liou <liou.vicki@gmail.com>
2022-05-18 23:24:57 -0300

    refact: add %#v into error message to print out struct with the values in its field

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -17,15 +17,24 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{
+			shape: Rectangle{Width: 12, Height: 6},
+			want:  72.0,
+		},
+		{
+			shape: Circle{Radius: 10},
+			want:  314.1592653589793,
+		},
+		{
+			shape: Triangle{Base: 12, Height: 6},
+			want:  36.0,
+		},
 	}
 
 	for _, tt := range tests {
 		got := tt.shape.Area()
 		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
 		}
 	}
 }
