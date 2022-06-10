1ccc8b5479815b7b70011f5c21edccc49b1e479a
Vicki Liou <liou.vicki@gmail.com>
2022-06-10 19:35:08 -0300

    fix(pointers): unchecked error

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -14,8 +14,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -35,6 +36,13 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Errorf("got an error but didn't want one")
 	}
 }
 
