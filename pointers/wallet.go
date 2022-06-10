c0cf87d7e8a421c4349cae1212b30f3014eaca2e
Vicki Liou <liou.vicki@gmail.com>
2022-06-10 18:27:19 -0300

    refact(pointers): add descriptive type

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,16 +1,19 @@
 package pointers
 
+// Bitcoin represents a number of Bitcoins.
+type Bitcoin int
+
 // Wallet stores the number of Bitcoin.
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit adds Bitcoin to the wallet.
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Balance returns the total of Bitcoin that a wallet has.
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
