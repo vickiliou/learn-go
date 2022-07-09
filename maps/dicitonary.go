07304d94ca7677c5492ff81062064700813b719a
Vicki Liou <liou.vicki@gmail.com>
2022-07-09 12:32:44 +0100

    refact(maps): check duplicated value

--- maps/dicitonary.go
+++ maps/dicitonary.go
@@ -5,8 +5,12 @@ import "errors"
 // Dictionary stores some words with their definitions.
 type Dictionary map[string]string
 
-// ErrNotFound means the definition could not be found for the given word.
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	// ErrNotFound means the definition could not be found for the given word.
+	ErrNotFound = errors.New("could not find the word you were looking for")
+	// ErrWordExists means the word that is trying to add already exists.
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 // Search find a word in the dictionary.
 func (d Dictionary) Search(word string) (string, error) {
@@ -18,7 +22,18 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-// Add inserts a word and definition into the dictionary.
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+// Add inserts a word and definition into the dictionary if the word doesn't exist.
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
