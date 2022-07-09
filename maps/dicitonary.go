8a03d071aa31afce8ab9d613789fbe76dcc6cd38
Vicki Liou <liou.vicki@gmail.com>
2022-07-09 12:12:27 +0100

    refact(maps): extract error into a variable

--- maps/dicitonary.go
+++ maps/dicitonary.go
@@ -5,11 +5,14 @@ import "errors"
 // Dictionary stores some words with their definitions.
 type Dictionary map[string]string
 
+// ErrNotFound means the definition could not be found for the given word.
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 // Search find a word in the dictionary.
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return definition, nil
