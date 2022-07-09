02b599323aff12b0d85e768ca521e743784a5267
Vicki Liou <liou.vicki@gmail.com>
2022-07-09 12:04:30 +0100

    refact(maps): add Dictionary type

--- maps/dicitonary.go
+++ maps/dicitonary.go
@@ -1,6 +1,9 @@
 package maps
 
+// Dictionary stores some words with their definitions.
+type Dictionary map[string]string
+
 // Search find a word in the dictionary.
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
