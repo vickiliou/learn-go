6feb256def3fc2519f84b3517b91fc25f247f3da
Vicki Liou <liou.vicki@gmail.com>
2022-07-09 12:09:11 +0100

    refact(maps): check if the word exists in the dictionary

--- maps/dicitonary.go
+++ maps/dicitonary.go
@@ -1,9 +1,16 @@
 package maps
 
+import "errors"
+
 // Dictionary stores some words with their definitions.
 type Dictionary map[string]string
 
 // Search find a word in the dictionary.
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,9 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unkown word", func(t *testing.T) {
+		_, err := dictionary.Search("unkown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
