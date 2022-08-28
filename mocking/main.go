f6171f4f0662bd84603cac358ca30bb6a95159ae
Vicki Liou <liou.vicki@gmail.com>
2022-08-28 15:52:31 -0300

    feat(mock): countdown function

---
+++ mocking/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+// Countdown prints a countdown from 3 and when it reaches zero it will print "Go!"
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
