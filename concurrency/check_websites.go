39dcd778f25436d54c722c1d9232e50abcfcbd7f
Vicki Liou <liou.vicki@gmail.com>
2022-11-18 18:20:12 -0300

    feat(concurrency): checkwebsite function

---
+++ concurrency/check_websites.go
@@ -0,0 +1,16 @@
+package concurrency
+
+// WebsiteChecker checks a url and returns a bool.
+type WebsiteChecker func(string) bool
+
+// CheckWebsites takes a WebsiteChecker and a slice of urls
+// and returns a map of each url checked.
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		results[url] = wc(url)
+	}
+
+	return results
+}
