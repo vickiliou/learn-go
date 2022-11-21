a38be5853288e7125f1e45c40ac925189ce9bc7b
Vicki Liou <liou.vicki@gmail.com>
2022-11-21 18:38:45 -0300

    refact(select): add timeout to Racer function

--- select/racer.go
+++ select/racer.go
@@ -1,16 +1,27 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
-// Racer compares the response times of a and b, returning the fastest one.
-func Racer(a, b string) (winner string) {
+var tenSecondTimeout = 10 * time.Second
+
+// Racer compares the response times of a and b, returning the fastest one, timing out after 10s.
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+// ConfigurableRacer compares the response times of a and b, returning the fastest one.
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
