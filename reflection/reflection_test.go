128ad4648bd5876e37bcb19e5604f38eeaf79b84
Vicki Liou <liou.vicki@gmail.com>
2022-12-09 16:50:26 -0300

    refact(reflection): check function

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -145,6 +145,23 @@ func TestWalk(t *testing.T) {
 			t.Errorf("got %v, want %v", got, want)
 		}
 	})
+
+	t.Run("with function", func(t *testing.T) {
+		aFunc := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aFunc, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
 }
 
 func assertContains(t testing.TB, haystack []string, needle string) {
